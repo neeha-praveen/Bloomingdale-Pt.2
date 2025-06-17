5a83d18253e64c1b9dc500f55370072c0f68a734
neeha-praveen <neeha.npraveen@gmail.com>
2025-06-17 12:21:08 +0530

    Add assets/ and index.html

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,15 @@
+import logo from './images/logo2.png'
+import add_icon from './images/white_add.png'
+import order_icon from './images/order_icon.png'
+import upload_area from './images/upload_area.png'
+import parcel_icon from './images/parcel_icon.png'
+import profile_image from './images/profile.png'
+
+export const assets = {
+    logo, 
+    add_icon,
+    order_icon,
+    upload_area,
+    parcel_icon,
+    profile_image
+}
