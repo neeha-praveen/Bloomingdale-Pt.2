36187fb6d80f90812728e896f5ace37c8a3f3279
neeha-praveen <neeha.npraveen@gmail.com>
2025-06-27 19:51:21 +0530

    Get url from .env

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,12 +5,13 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    const url = import.meta.env.VITE_BACKEND_URL;
+
     const [token, setToken] = useState('');
     const [products_all, setProductsAll] = useState([]);
 
     const addToCart = async (itemId) => {
-        const updatedCart = { ...cartItems };
+        const updatedCart = { ...cartItems };s
 
         if (!updatedCart[itemId]) {
             updatedCart[itemId] = 1;
