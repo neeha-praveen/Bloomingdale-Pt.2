13a500b01f4bc79bee53e5d8920caab126448b8f
neeha-praveen <neeha.npraveen@gmail.com>
2025-06-27 20:47:35 +0530

    Modify db.js and auth.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose"
+import dotenv from "dotenv";
+dotenv.config();
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://neehanpraveen:abcabc@cluster0.glvhugt.mongodb.net/bloomingdale').then(()=>console.log("db connected"));
+    await mongoose.connect(process.env.MONGODB_URI).then(() => console.log("db connected"));
 }
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,4 +1,6 @@
 import jwt from 'jsonwebtoken'
+import dotenv from "dotenv";
+dotenv.config();
 
 const authMiddleware = async (req,res,next) => {
     const {token} = req.headers;
